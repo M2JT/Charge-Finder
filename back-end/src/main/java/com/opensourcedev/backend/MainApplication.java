ffe186d27e55c89a6febb8b80cd86bfd378651fe
M2JT <m2jt998@gmail.com>
2023-12-13 20:47:38 -0500

    update tables + add back-end logic to get all stations info

--- back-end/src/main/java/com/opensourcedev/backend/MainApplication.java
+++ back-end/src/main/java/com/opensourcedev/backend/MainApplication.java
@@ -1,9 +1,11 @@
 package com.opensourcedev.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.opensourcedev.backend.mapper")
 public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);

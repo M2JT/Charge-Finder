c1765884898fc21c140888ec33d762b1d57b4962
ZFarooqi24 <zayyan.farooqi24@gmail.com>
2023-12-17 22:08:23 -0500

    Revert Front-end code back to unaltered version from main.

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -64,33 +64,36 @@ const Login = () => {
 export default Login;
 
 export async function action({ request }) {
-  const formData = await request.formData();
-  console.log('Email:', formData.get('email'));
-  console.log('Password:', formData.get('password'));
-  const email = formData.get('email');
-  const password = formData.get('password');
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
 
-  if (!email || !password) {
-    alert("Please enter both email and password.");
-    return;
-  }
-
-  const authData = { email, password };
-
-  const response = await fetch('http://localhost:8080/login', {
+  const response = await fetch('http://localhost:8080/' + 'login', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(authData),
   });
 
-  if (!response.ok) {
-    const text = await response.text();
-    alert(text);
-    return null;
+  if (response.status === 422 || response.status === 401) {
+    return response;
   }
 
-  alert('Login successful.');
-  localStorage.setItem('username', email);
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem('expiration', expiration.toISOString());
+  localStorage.setItem('username','admin');
+
   return redirect('/');
 }
-
--- front-end/src/components/AuthForm.jsx
+++ front-end/src/components/AuthForm.jsx
@@ -1,34 +1,12 @@
 import { useState } from "react";
+import { Form, useActionData, useNavigation } from "react-router-dom";
 import classes from "../css/AuthForm.module.css";
-
 export default function AuthForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const authData = { email, password };
-
-    const response = await fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(authData),
-    });
-
-    if (!response.ok) {
-      const text = await response.text();
-      alert(text);
-    } else {
-      alert('Login successful.');
-      localStorage.setItem('username', email);
-      // Redirect or update state as needed
-    }
-  };
-
+  const data = useActionData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <form onSubmit={handleSubmit} className={classes.loginForm}>
-      {/* Email input */}
+    <Form method="post" className={classes.loginForm}>
       <div className={classes.loginUsernameEmailForm}>
         <label htmlFor="email">Email</label>
         <input
@@ -37,29 +15,41 @@ export default function AuthForm() {
           name="email"
           placeholder="Enter Email"
           required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
         />
+        {/* <Form.Control
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        /> */}
       </div>
-
-      {/* Password input */}
       <div className={classes.loginPasswordForm}>
-        <label htmlFor="password">Password</label>
+        <label htmlFor="image">Password</label>
         <input
           id="password"
           type="password"
           name="password"
           placeholder="Enter Password"
           required
+        />
+        {/* <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        />
+        /> */}
       </div>
-
-      {/* Submit button */}
-      <button type="submit" className={classes.loginSubmitBtn}>
-        Login
+      <button className={classes.loginSubmitBtn} disabled={isSubmitting}>
+        {isSubmitting ? "Submitting..." : "Login"}
       </button>
-    </form>
+      {data && data.errors && (
+        <ul className={classes.loginFormError}>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
+      {data && data.message && <p className={classes.loginFormErrorMsg}>{data.message}</p>}
+    </Form>
   );
 }

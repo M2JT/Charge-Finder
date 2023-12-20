3020c2202f0f78647e8f7f8c127092b94ee44bf8
M2JT <m2jt998@gmail.com>
2023-12-20 18:46:26 -0500

    finalize README + add back-end unit tests + delete dummy back-end

--- back-end/src/test/java/com/opensourcedev/backend/MainTest.java
+++ back-end/src/test/java/com/opensourcedev/backend/MainTest.java
@@ -1,12 +1,16 @@
 package com.opensourcedev.backend;
 
 import com.opensourcedev.backend.controller.MainController;
+import com.opensourcedev.backend.dto.RentalDetail;
+import com.opensourcedev.backend.model.ChargingStation;
 import com.opensourcedev.backend.model.Rental;
 import com.opensourcedev.backend.service.MainService;
+
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -28,8 +32,8 @@ class MainTest {
     void testGetRentalHistory() {
         String username = "test_user";
         List<Rental> mockRentalHistory = Arrays.asList(
-            new Rental(10,27, 987654321, new Date(1640007900000L), 0, 9, "Returned", 2),
-            new Rental(11,27, 567892341, new Date(1639926060000L), 1, 18, "Returned", 2)
+                new Rental(10, 27, 987654321, new Date(1640007900000L), 0, 9, "Returned", 2),
+                new Rental(11, 27, 567892341, new Date(1639926060000L), 1, 18, "Returned", 2)
         );
         when(mainService.getRentalHistory(username)).thenReturn(mockRentalHistory);
 
@@ -48,5 +52,48 @@ class MainTest {
             assertEquals(expectedRental.getRentalStatus(), actualRental.getRentalStatus());
             assertEquals(expectedRental.getChargingStationId(), actualRental.getChargingStationId());
         }
+    }
+
+    @Test
+    void testGetAllStationsInfo() {
+        List<ChargingStation> mockStations = getChargingStations();
+
+        when(mainService.getAllStationsInfo()).thenReturn(mockStations);
+
+        List<ChargingStation> result = mainController.getAllStations();
+
+        assertEquals(mockStations, result);
+    }
+
+    @Test
+    void rentPowerBankSuccessful() {
+        RentalDetail rentalDetail = new RentalDetail("test_user", 123, new Date(), 2, 8, "Rented", 5);
+
+        when(mainService.rentPowerBank(rentalDetail)).thenReturn(true);
+
+        String result = mainController.rentPowerBank(rentalDetail);
+
+        assertEquals("rent successful!", result);
+    }
+
+    @Test
+    void rentPowerBankFailed() {
+        RentalDetail rentalDetail = new RentalDetail("test_user", 456, new Date(), 3, 10, "Rented", 8);
+
+        when(mainService.rentPowerBank(rentalDetail)).thenReturn(false);
+
+        String result = mainController.rentPowerBank(rentalDetail);
+
+        assertEquals("rent failed!", result);
+    }
+
+    @NotNull
+    private static List<ChargingStation> getChargingStations() {
+        ChargingStation station1 = new ChargingStation(1, "6 metro tech center", 40.694292, -73.985205, 18, 9);
+        ChargingStation station2 = new ChargingStation(2, "random place", 40.6933639, -73.9704101, 12, 6);
+
+        List<ChargingStation> mockStations = Arrays.asList(station1, station2);
+
+        return mockStations;
     }
 }
